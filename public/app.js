41e645335893a38d3dd36b0d23d122c539cbee7d
bulletbornil-tech <bulletbornil@gmail.com>
2026-06-13 11:35:37 +0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,33 +1,60 @@
-const list = document.getElementById("list");
-const video = document.getElementById("video");
-const title = document.getElementById("title");
+const video = document.getElementById("videoPlayer");
+const channelList = document.getElementById("channelList");
+const nowPlaying = document.getElementById("nowPlaying");
+const search = document.getElementById("search");
 
-fetch("/api/channels")
+let channels = [];
+
+// Load JSON
+fetch("channels.json")
   .then(res => res.json())
   .then(data => {
-    data.forEach(ch => {
-      let div = document.createElement("div");
-      div.className = "channel";
-
-      div.innerHTML = `
-        <img src="${ch.logo}">
-        <span>${ch.name}</span>
-      `;
-
-      div.onclick = () => play(ch);
-      list.appendChild(div);
-    });
+    channels = data;
+    renderChannels(channels);
   });
 
-function play(ch) {
-  title.innerText = ch.name;
+// Render channels
+function renderChannels(list) {
+  channelList.innerHTML = "";
+
+  list.forEach(ch => {
+    const div = document.createElement("div");
+    div.className = "channel";
+
+    div.innerHTML = `
+      <img src="${ch.logo}" />
+      <p>${ch.name}</p>
+    `;
+
+    div.onclick = () => playChannel(ch);
+
+    channelList.appendChild(div);
+  });
+}
+
+// Play channel (HLS fix)
+function playChannel(channel) {
+  nowPlaying.innerText = "Playing: " + channel.name;
 
   if (Hls.isSupported()) {
     const hls = new Hls();
-    hls.loadSource(ch.url);
+    hls.loadSource(channel.stream_url);
     hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
-  } else {
-    video.src = ch.url;
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play();
+    });
+  }
+  else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    video.src = channel.stream_url;
+    video.play();
   }
 }
+
+// Search filter
+search.addEventListener("input", () => {
+  const value = search.value.toLowerCase();
+  const filtered = channels.filter(ch =>
+    ch.name.toLowerCase().includes(value)
+  );
+  renderChannels(filtered);
+});
